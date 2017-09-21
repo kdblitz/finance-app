2d6a9dbf79568931bdf551e5707b96475ee14318
Ken Lee <kdblitz@gmail.com>
2017-09-21 20:53:24 +0800

    Subtotal displayed if special rows were included on expense sheet.

--- src/components/expenses/expense_form.js
+++ src/components/expenses/expense_form.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchExpenseData, saveExpenseData, removeUser } 
-    from '../../actions/expense_actions';
+import { fetchExpenseData, saveExpenseData, removeUser }
+  from '../../actions/expense_actions';
 
 import './expense_form.css';
 import AddUserForm from './add_user_form';
@@ -16,106 +16,113 @@ import ServiceChargeRow from './rows/service_charge_row';
 import TotalRow from './rows/total_row';
 
 const rows = {
-    SubtotalRow,
-    ServiceChargeRow,
+  SubtotalRow,
+  ServiceChargeRow,
 }
 
 class ExpenseForm extends Component {
-    componentDidMount() {
-        this.props.fetchExpenseData();
+  componentDidMount() {
+    this.props.fetchExpenseData();
+  }
+
+  render() {
+    return (
+      <div className="Expense-form">
+        <h2>Expense form</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              {this.renderItemHeader()}
+              <th className="shared">Shared?</th>
+              {this.renderNameHeader()}
+              <th className="addUser">
+                <AddUserForm />
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderBody()}
+            {this.renderSpecialRows()}
+            {this.renderItemAdderRow()}
+            {this.renderFooterRows()}
+          </tbody>
+        </table>
+        <button className="btn btn-primary"
+          onClick={() => this.saveExpense()}>Save</button>
+      </div>
+    );
+  }
+
+  renderItemHeader() {
+    return _.map(['Name', 'Quantity', 'Price'],
+      name => <th key={name} className={name.toLowerCase()}>{name}</th>);
+  }
+
+  renderNameHeader() {
+    return _.keys(this.props.CurrentExpense.users).map(user => {
+      return (
+        <th key={user} className="users">
+          <span>{user}</span>
+          <button className="btn btn-danger"
+            onClick={() => this.props.removeUser(user)}>-</button></th>
+      );
+    });
+  }
+
+  renderBody() {
+    return _.map(this.props.CurrentExpense.items, item =>
+      <ItemRow key={item.name} item={item} users={this.props.CurrentExpense.users} />);
+  }
+
+  renderSpecialRows() {
+    const currentRows = _.cloneDeep(this.props.CurrentExpense.rows) || [];
+    if (currentRows.length) {
+      currentRows.push('SubtotalRow');
     }
 
-    render() {
-        return (
-            <div className="Expense-form">
-                <h2>Expense form</h2>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            {this.renderItemHeader()}
-                            <th className="shared">Shared?</th>
-                            {this.renderNameHeader()}
-                            <th className="addUser">
-                                <AddUserForm />
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderBody()}
-                        {this.renderSpecialRows()}
-                        {this.renderItemAdderRow()}
-                        {this.renderFooterRows()}
-                    </tbody>
-                </table>
-                <button className="btn btn-primary"
-                    onClick={()=>this.saveExpense()}>Save</button>
-            </div>
-        );
-    }
+    const specialRows = _.map(currentRows, row => {
+      const SpecialRow = rows[row];
+      return (
+        <SpecialRow key={row} expenseData={this.props.CurrentExpense}
+          computations={this.props.Computations} />
+      );
+    });
 
-    renderItemHeader() {
-        return _.map(['Name', 'Quantity', 'Price'], 
-            name => <th key={name} className={name.toLowerCase()}>{name}</th>);
-    }
+    return specialRows;
+  }
 
-    renderNameHeader() {
-        return _.keys(this.props.CurrentExpense.users).map(user => {
-            return (
-                <th key={user} className="users">
-                    <span>{user}</span>
-                    <button className="btn btn-danger"
-                        onClick={() => this.props.removeUser(user)}>-</button></th>
-            );
-        });
-    }
+  renderFooterRows() {
+    return [
+      <TotalRow expenseData={this.props.CurrentExpense} computations={this.props.Computations} />,
+      <PaymentRow users={this.props.CurrentExpense.users} computations={this.props.Computations} />,
+      <ChangeRow expenseData={this.props.CurrentExpense} computations={this.props.Computations} />
+    ];
+  }
 
-    renderBody() {
-        return _.map(this.props.CurrentExpense.items, item =>
-            <ItemRow key={item.name} item={item} users={this.props.CurrentExpense.users} />);
-    }
+  renderItemAdderRow() {
+    return <tr><td colSpan="3"><AddItemForm /></td></tr>;
+  }
 
-    renderSpecialRows() {
-        return _.map(this.props.CurrentExpense.rows, row => {
-            const SpecialRow = rows[row];
-            return (
-                <SpecialRow key={row} expenseData={this.props.CurrentExpense} 
-                    computations={this.props.Computations} />
-            );
-        });
-    }
-
-    renderFooterRows() {
-        return [
-            <TotalRow expenseData={this.props.CurrentExpense} computations={this.props.Computations} />,
-            <PaymentRow users={this.props.CurrentExpense.users} computations={this.props.Computations} />,
-            <ChangeRow expenseData={this.props.CurrentExpense} computations={this.props.Computations} />
-        ];
-    }
-
-    renderItemAdderRow() {
-        return <tr><td colSpan="3"><AddItemForm /></td></tr>;
-    }
-
-    saveExpense() {
-        const { users, items, rows } = this.props.CurrentExpense; 
-        this.props.saveExpenseData({
-            users,
-            items,
-            rows
-        });
-    }
+  saveExpense() {
+    const { users, items, rows } = this.props.CurrentExpense;
+    this.props.saveExpenseData({
+      users,
+      items,
+      rows
+    });
+  }
 
 }
 
-function mapStateToProps({CurrentExpense, Computations}) {
-    return {
-        CurrentExpense,
-        Computations
-    };
+function mapStateToProps({ CurrentExpense, Computations }) {
+  return {
+    CurrentExpense,
+    Computations
+  };
 }
 
 export default connect(mapStateToProps, {
-    fetchExpenseData,
-    saveExpenseData,
-    removeUser
+  fetchExpenseData,
+  saveExpenseData,
+  removeUser
 })(ExpenseForm);
