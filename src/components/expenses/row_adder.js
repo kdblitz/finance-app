f042ff7e7da79f2f2057f6d88b8ea9a23682bcc7
Ken Lee <kdblitz@gmail.com>
2017-09-21 15:25:13 +0800

    Created own webpack configuration

---
+++ src/components/expenses/row_adder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RowAdder extends Component {
+    render() {
+        return (
+            <div className="btn-group">
+                <button type="button" className="btn btn-success dropdown-toggle"></button>
+            </div>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const publicPath = '/';
+
+module.exports = {
+  entry: [
+    // require.resolve('webpack')
+    // require.resolve('./polyfills'),
+    './src'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: publicPath
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx'],
+    plugins: [
+      // new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: path.resolve(__dirname, 'public', 'index.html')
+      })
+    ]
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      // {
+      //     test: /\.(js|jsx)$/,
+      //     enforce: 'pre',
+      //     use: [{
+      //         options: {
+      //             formatter: {
+      //                 //setup formatter
+      //             }
+      //         }
+      //     }]
+      // }, 
+      {
+        oneOf: [
+          {
+            test: /\.(js|jsx)$/,
+            loader: require.resolve('babel-loader'),
+            options: {
+              babelrc: false,
+              presets: ["env", "react"],
+              cacheDirectory: true
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1
+                }
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9'
+                      ],
+                      flexbox: 'no-2009'
+                    })
+                  ]
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
